e2115a0351039b90169671c79e33c7bc788c6e05
0x4bd0 <abdoumjr@gmail.com>
2020-11-23 17:30:52 +0100

    bloc setup, feeling freaking dizy

--- lib/book.dart
+++ lib/book.dart
@@ -3,21 +3,26 @@ class Book {
 String _id;
 String _title;
 String _author;
+int _rank;
 
-Book(this._id,this._title,this._author);
+Book(this._id,this._title,this._author,this._rank);
 
 set id(String id){
   this._id = id;
 }
-set title(String _title){
-  this._title = _title;
+set title(String title){
+  this._title = title;
 }
-set author(String _author){
-  this._author = _author;
+set author(String author){
+  this._author = author;
+}
+set rank(int rank){
+  this._rank = rank;
 }
 
 String get id => this._id;
 String get author => this._author;
 String get title => this._title;
+int get rank => this._rank;
 
 }
---
+++ lib/book_block.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'book.dart';
+
+class BookBlock {
+
+  List<Book> _booksList = [
+    Book("001","Harry potter","John Doe",1),
+    Book("002","possitive thinking","7amouda",2),
+    Book("003","The art of not giving a F","ahmed el bodi",3)
+  ];
+
+  // sink : add data in stream
+  // stream : the stream lel
+  // source : the listner point ? i guess xD
+
+
+  final _booksListStreamController = StreamController<List<Book>>(
+
+  );
+
+  final _rankUpBookStreamController = StreamController<List<Book>>(
+
+  );
+
+  final _rankDownBookStreamController = StreamController<List<Book>>(
+
+  );
+
+
+  // stream getters ?
+
+  Stream<List<Book>> get booksListStream => _booksListStreamController.stream;
+
+  StreamSink<List<Book>> get booksListSink => _booksListStreamController.sink;
+
+  StreamSink<List<Book>> get rankUpBookSink => _rankUpBookStreamController.sink;
+
+  StreamSink<List<Book>> get rankDownBookSink => _rankDownBookStreamController.sink;
+
+
+  // methods
+   
+   _rankUp(Book book) {
+
+    int rank = book.rank;
+    int new_rank = rank++;
+
+    final index = _booksList.indexWhere((item) => item.id == book.id );
+
+    _booksList[index].rank = new_rank;
+
+    booksListSink.add(_booksList);
+
+   }
+
+   _rankDown(Book book) {
+     
+    int rank = book.rank;
+    int new_rank = rank--;
+
+    final index = _booksList.indexWhere((item) => item.id == book.id );
+
+    _booksList[index].rank = new_rank;
+
+    booksListSink.add(_booksList);
+
+   }
+
+  // constructor
+  BookBlock(){
+
+    _booksListStreamController.add(_booksList);
+
+    _rankUpBookStreamController.stream.listen(_rankUp);
+
+    _rankDownBookStreamController.stream.listen(_rankDown);
+
+  }
+
+
+  void dispose() {
+      _booksListStreamController.close();
+      _rankUpBookStreamController.close();
+      _rankDownBookStreamController.close();
+  }
+  
+}
